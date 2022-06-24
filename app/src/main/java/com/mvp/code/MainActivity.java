a7c3e99993138f104969831e52a38c210aa847d9
pzh2014 <378276746@qq.com>
2022-06-24 16:33:56 +0800

    init

---
+++ app/src/main/java/com/mvp/code/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mvp.code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
